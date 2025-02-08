e1ce638b67b59c61c640e64eff376f6d0ff47b90
Developer <shourya.saxena@ens.enterprises>
2025-02-08 10:45:14 +0530

    Revert "test nodejs backened folder structure"
    This reverts commit 2ffaf92ed1282781822956109ea96a4c6eb40b67.

--- routes/UserRoutes/userRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-const router = express.Router();
-import { register, login } from '../../controllers/UserController/userController.js';
-
-
-router.post('/register', register);
-router.post('/login', login);
-
-export default router;
